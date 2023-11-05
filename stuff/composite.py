f46cebabc6dd7b9a064fcecec00352b96c026356
avt05 <adrianvtran@Adrians-MacBook-Air.local>
2023-11-05 00:48:00 -0500

    finalized composite.py to fully read the data set

--- stuff/composite.py
+++ stuff/composite.py
@@ -94,74 +94,61 @@ def runCounter(data, file):
             if (rowIndex != 0):
                 data.append((row[0]).split(','))
             rowIndex += 1
-    counter1 = 0
-    counter2 = 0
-    counter3 = 0
-    counter4 = 0
+    checkScoreGood = 0
+    checkScoreBad = 0
+    checkLTVGood = 0
+    checkLTVBad = 0
+    checkDTIGood = 0
+    checkDTIBad = 0
+    checkFEDTIGood = 0
+    checkFEDTIBad = 0
+    checkTotalGood = 0
+    checkTotalBad = 10000
     for u in range(10000):
         Buyer = User(int(data[u][0]),float(data[u][1]),float(data[u][2]),float(data[u][3]),float(data[u][4]),
                     float(data[u][5]),float(data[u][6]),float(data[u][7]),float(data[u][8]),int(data[u][9]))
         # run different counters
-        counter1 += 1
-        counter2 += 1
-        counter3 += 1
-        counter4 += 1
+        
+        # validate creditScore
+        checkScore = Buyer.evalCreditScore()
+        if (checkScore == False):
+            checkScoreBad += 1
+        else:
+            checkScoreGood += 1
+            
+        # validate LTV
+        LTV = Buyer.calcLTV()
+        percLTV = evalLTV(LTV)
+        if (percLTV == False):
+            PMI = Buyer.calcPMI()
+            checkLTVBad += 1
+        else:
+            PMI = 0
+            checkLTVGood += 1
+            
+        # validate DTI
+        monthlyDebt = calcMonthlyDebt(Buyer.carPayment, Buyer.loanPayment, Buyer.mortgage, Buyer.monthlyCardPayment, PMI)
+        DTI = calcDTI(monthlyDebt, Buyer.monthlyIncome)
+        if (DTI <= 36):
+            checkDTIGood += 1
+        else:
+            checkDTIBad += 1
+            
+        # validate FEDTI
+        FEDTI = calcFEDTI(Buyer.mortgage, Buyer.monthlyIncome, PMI)
+        percFEDTI = evalFEDTI(FEDTI)
+        if (percFEDTI == False):
+            checkFEDTIBad += 1
+        else:
+            checkFEDTIGood += 1
+
+        if(percFEDTI == True and DTI <= 36 and percLTV == True and checkScore == True):
+            checkTotalGood += 1
         del Buyer
-    counters = [counter1, counter2, counter3, counter4]
+
+    checkTotalBad -= checkTotalGood
+    counters = [checkScoreGood, checkScoreBad, checkLTVGood, checkLTVBad, checkDTIGood, checkDTIBad, checkFEDTIGood, checkFEDTIBad, checkTotalGood, checkTotalBad]
 
     return counters
 
-useSampleInput = False
-askUser = ""
-while (askUser != "Y" or askUser != "N"):
-    askUser = input("View samples? [Y/N]: ")
-    if (askUser == "Y"):
-        useSampleInput = True
-        readFile(data, "HackUTD-2023-HomeBuyerInfo.csv")
-        Buyer = lookupUserSample(data)
-        break
-    elif (askUser == "N"):
-        useSampleInput = False
-        Buyer = createUser()
-        break
-    else:
-        print("Please enter valid input!")
-
-
-# validate creditScore
-checkScore = Buyer.evalCreditScore()
-if (checkScore == False):
-    print('Your credit score is low, you should improve it.')
-else:
-    print('Your credit score is good!')
-
-# validate LTV
-LTV = Buyer.calcLTV()
-percLTV = evalLTV(LTV)
-if (percLTV == False):
-    PMI = Buyer.calcPMI()
-    print('Your LTV is high, so you will need to get PMI')
-else:
-    PMI = 0
-    print('Your LTV is good!')
-
-# validate DTI
-monthlyDebt = calcMonthlyDebt(Buyer.carPayment, Buyer.loanPayment, Buyer.mortgage, Buyer.monthlyCardPayment, PMI)
-DTI = calcDTI(monthlyDebt, Buyer.monthlyIncome)
-
-if (DTI <= 36):
-    print('DTI is good')
-else:
-    print('DTI is bad')
-    
-# validate FEDTI
-
-FEDTI = calcFEDTI(Buyer.mortgage, Buyer.monthlyIncome, PMI)
-percFEDTI = evalFEDTI(FEDTI)
-
-if (percFEDTI == False):
-    print('FEDTI is bad')
-else:
-    print('FEDTI is good')
-
-    
+print(runCounter(data, "HackUTD-2023-HomeBuyerInfo.csv"))
